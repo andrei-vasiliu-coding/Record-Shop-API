89615ad0d7ca33a1373903da90cfe8d5d0006eb4
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-09-05 11:02:13 +0100

    feat: create project

---
+++ recordShop/src/test/java/com/northcoders/recordShop/RecordShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
