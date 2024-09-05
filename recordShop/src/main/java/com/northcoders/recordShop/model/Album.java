a332a2cfd6ea063a1db9b50f46e993cc85979425
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-09-05 11:57:46 +0100

    feat: complete Album class

--- recordShop/src/main/java/com/northcoders/recordShop/model/Album.java
+++ recordShop/src/main/java/com/northcoders/recordShop/model/Album.java
@@ -1,4 +1,34 @@
 package com.northcoders.recordShop.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Album {
+
+    @Id
+    @Column(updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "album_seq")
+    @SequenceGenerator(name = "album_seq", allocationSize = 1)
+    Long id;
+
+    @Column
+    String title;
+
+    @Column
+    String artist;
+
+    @Column
+    Genre genre;
+
+    @Column
+    String description;
+
 }
