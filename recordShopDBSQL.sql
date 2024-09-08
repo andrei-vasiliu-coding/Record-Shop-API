1a8e117e262372b71267e394c292131cc0ab983c
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-09-08 20:17:04 +0100

    feat: implement postgresql option

---
+++ recordShopDBSQL.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.albums(
+	title, artist, genre, "releaseYear", description)
+	VALUES ("Future Nostalgia", "Dua Lipa", "Pop", 2020, "An amazing pop album that saved me through the pandemic!!"),
+	("Materia Prisma", "Marco Mengoni", "Pop", 2023, "Such a talented singer, took over the stage at Eurovision"),
+	("Un Verano Sin Ti", "Bad Bunny", "Latin music", 2022, "Latin-American music perfect for parties!");
